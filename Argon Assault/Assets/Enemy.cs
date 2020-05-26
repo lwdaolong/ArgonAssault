823782eaa69c88d7a3d0f11ec43c799de989948b
Logan Wang <loganwang19@gmail.com>
2020-05-25 20:16:15 -0700

    ScoreBoard Setup

---
+++ Argon Assault/Assets/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathfx;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddComponents();
+    }
+
+    private void AddComponents()
+    {
+        BoxCollider col = gameObject.AddComponent<BoxCollider>();
+        col.isTrigger = false;
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        deathfx.SetActive(true);
+        GetComponent<Rigidbody>().useGravity = true;
+        Invoke("DestroyOBJ", 1f);
+
+
+    }
+
+    void DestroyOBJ()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Argon Assault/Assets/ScoreBoard.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour
+{
+    int score;
+    Text scoreText;
+
+    [SerializeField] int defaultScore = 12;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        scoreText.text = score.ToString();
+    }
+
+    public void addScore()
+    {
+        score += defaultScore;
+        scoreText.text = score.ToString();
+    }
+
+    public void addScore(int specialScore)
+    {
+        score += specialScore;
+        scoreText.text = score.ToString();
+    }
+
+}
